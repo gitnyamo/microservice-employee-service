972d0cc6d2114374e6243a53ae64d2b2c5be7daa
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-06-14 21:11:46 -0400

    notes for making the call using REST Template

--- src/main/java/fun/services/asap/employeeservice/controller/EmployeeController.java
+++ src/main/java/fun/services/asap/employeeservice/controller/EmployeeController.java
@@ -8,9 +8,13 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public String getEmployee(){
-        String address = null;
+        //get the data from address service by hitting the address url:http://localhost:8080/address
+        //make an external REST API call
+        //In order to make a hit to the address server I need a REST Client. To make this call, we need a rest client
+        // and this case here, we will be using a rest client called REST Template
+        String address = null;// initialized with null here at the moment.
         //return address data along with the employee data
-        return "Name: Kisha Baraka, email: kishab@gmail.com" + address;
+        return "Name: Kisha Baraka, email: kishab@gmail.com " + address;
     }
 
 }
