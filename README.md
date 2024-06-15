6e413ccf5ea195c53463700a3cb83ca052834632
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-06-14 22:04:21 -0400

    readme file created for brief into and will be updated as we keep on going

---
+++ README.md
@@ -0,0 +1,13 @@
+# Example Flow
+
+1. A client makes a GET request to http://localhost:8080/employees.
+2. The getEmployee() method is invoked.
+3. Inside this method, a GET request is made to http://localhost:8080/address to fetch an address.
+4. The address is obtained and stored in the address variable.
+5. A string containing a name, email, and the fetched address is returned as the response.
+## Dependencies
+<span style="color:lawngreen"> *restTemplate*</span>: This is to be an instance of RestTemplate created as a bean to be autowired, a class provided by Spring to make REST calls which getting deprecated soon. It is typically auto-configured and injected into the controller(@bean created under config folder at the moment).
+
+http://localhost:8080/address: This endpoint is anothe(address endpong REST endpoint available in the the address module/application or server, which is being called from the employee application using restTemplate and returns an address in the response body.
+
+Note that for the code to work, the RestTemplate instance needs to be defined and injected by autowire into the controller class.... We created a bean for it to work 
--- src/main/java/fun/services/asap/employeeservice/controller/EmployeeController.java
+++ src/main/java/fun/services/asap/employeeservice/controller/EmployeeController.java
@@ -11,11 +11,10 @@ public class EmployeeController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/employees")
-    public String getEmployee(){
-        String address = restTemplate.getForObject("http://localhost:8080/address",String.class);
-        return "Name: Kisha Baraka, email: kishab@gmail.com " + address;
-        // employee service is now internally connected with the address service and fetching the address service data by making a call using the restTemplate
+    @GetMapping("/employees")//This annotation maps HTTP GET requests to the /employees endpoint to this method. When a client sends a GET request to http://localhost:8080/employees, this method will be invoked.
+    public String getEmployee(){//This is the method signature. The method is public and returns a String.
+        String address = restTemplate.getForObject("http://localhost:8080/address",String.class);//This line uses restTemplate to make a GET request to another endpoint (http://localhost:8080/address) on the same server. The getForObject method fetches the response body and maps it to a String. The response from the /address endpoint is stored in the address variable.
+        return "Name: Kisha Baraka, email: kishab@gmail.com " + address;//This line constructs a response string that includes a hardcoded name and email along with the address fetched from the /address endpoint. This concatenated string is then returned as the response to the original GET request to /employees.
     }
 
 }
