060def8a9ca24537107d8ef4706d0f95aa43a395
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-06-17 15:43:59 -0400

    created entity class--pojo

---
+++ src/main/java/fun/services/asap/employeeservice/entity/Employee.java
@@ -0,0 +1,10 @@
+package fun.services.asap.employeeservice.entity;
+
+public class Employee {
+   private Long empId;
+   private String empFirstName;
+   private String empLastName;
+   private int empAge;
+   private String empEmail;
+
+}
