3f787b0d5da2527c180d8c44d3a1a94078cf0608
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-06-18 14:36:49 -0400

    model mapper and dispatcher config comments removed

--- src/main/java/fun/services/asap/employee/config/EmployeeConf.java
+++ src/main/java/fun/services/asap/employee/config/EmployeeConf.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration// makes dispatcher servelet to create the Beans whenever called for use.
+@Configuration
 public class EmployeeConf {
 
     @Bean
@@ -14,7 +14,7 @@ public class EmployeeConf {
     }
 
     @Bean
-    public ModelMapper modelMapper(){// model mapper bean is a third part api and not created by spring so no spring model mapper bean injection.
-        return new ModelMapper();// created to be used for modelMapping instead of the setters of field.
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
