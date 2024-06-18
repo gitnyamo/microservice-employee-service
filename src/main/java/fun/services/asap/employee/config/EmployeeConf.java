086d9dfb8d877c1a3b2fc8bbac528231d198a047
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-06-18 14:26:10 -0400

    modelmapper implement inplace of setters... id uniquely maps all fields between the two classes all the way

--- src/main/java/fun/services/asap/employee/config/EmployeeConf.java
+++ src/main/java/fun/services/asap/employee/config/EmployeeConf.java
@@ -1,5 +1,6 @@
 package fun.services.asap.employee.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +11,10 @@ public class EmployeeConf {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();// created to be used for modelMapping instead of the setters of field.
     }
 }
--- src/main/java/fun/services/asap/employee/service/EmployeeService.java
+++ src/main/java/fun/services/asap/employee/service/EmployeeService.java
@@ -3,6 +3,7 @@ package fun.services.asap.employee.service;
 import fun.services.asap.employee.entity.Employee;
 import fun.services.asap.employee.repo.EmployeeRepository;
 import fun.services.asap.employee.response.EmployeeResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +13,21 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-        public EmployeeResponse getEmployeeById(int id){
+    @Autowired
+    private ModelMapper modelMapper;// to further use the model mapper just like RestTemplate we have to create a bean for it and mark the instance method with @Bean and the class for the bean(s) to be marked as @Configuration.. see the class for the beans under the config folder.
+
+    public EmployeeResponse getEmployeeById(int id) {
+        //note: here instead of using setters when making a db call from the repository, now use the model mapper to replace the setters... It will be quicker and do once for the object mapping employee entity object to the employeeResponse object instead of each field at a time when using the setters.
         Employee employee = employeeRepository.findById(id).get();
-        // note that inside here i am doing conversion manually using setters. However, when you have so many fields it can be so hard to do that to resolve this, I will be using ModelMapper moving forward for this app...
-        EmployeeResponse employeeResponse = new EmployeeResponse();
-        employeeResponse.setId(employee.getId());
-        employeeResponse.setFirstName(employee.getLastName());
-        employeeResponse.setLastName(employee.getLastName());
-        employeeResponse.setEmail(employee.getEmail());
-        employeeResponse.setAge(employee.getAge());
-        employeeResponse.setGender(employee.getGender());
+        EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);//note: here you are mapping from employee as the source class for the employee fields to map to the EmployeeResponse class... work of the model mapper.... swift and easy..  then assign it to the new local variable of the employeeResponse as seen here on this line code.
+
+//        EmployeeResponse employeeResponse = new EmployeeResponse();
+//        employeeResponse.setId(employee.getId());
+//        employeeResponse.setFirstName(employee.getLastName());
+//        employeeResponse.setLastName(employee.getLastName());
+//        employeeResponse.setEmail(employee.getEmail());
+//        employeeResponse.setAge(employee.getAge());
+//        employeeResponse.setGender(employee.getGender());
 
         return employeeResponse;
     }
